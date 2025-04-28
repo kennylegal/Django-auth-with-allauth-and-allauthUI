427acfb56156c876ef361fc3a8d048e80e90c4d9
kennylegal <legalspace02@gmail.com>
2025-04-28 23:36:16 +0100

    Added a rough Html ro the Home page

--- allauthProject/urls.py
+++ allauthProject/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from .views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('', home, name='home'), 
 ]
